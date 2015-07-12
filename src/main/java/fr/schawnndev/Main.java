51b9da7bf02f8d5fd909421c5facbd0974870e53
SchawnnDev <iperskill@gmail.com>
2015-07-12 12:12:34 +0200

    First commit

---
+++ src/main/java/fr/schawnndev/Main.java
@@ -0,0 +1,19 @@
+/*
+ * ******************************************************
+ *  * Copyright (C) 2015 SchawnnDev <contact@schawnndev.fr>
+ *  *
+ *  * This file (fr.schawnndev.Main) is part of HGStatus.
+ *  *
+ *  * Created by SchawnnDev on 12/07/15 12:08.
+ *  *
+ *  * HGStatus can not be copied and/or distributed without the express
+ *  * permission of SchawnnDev.
+ *  ******************************************************
+ */
+
+package fr.schawnndev;
+
+import java.applet.Applet;
+
+public class Main extends Applet {
+}
